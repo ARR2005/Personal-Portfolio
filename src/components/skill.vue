e647fbf378f3226976217ec8898c7e4458515ed8
Aldwin Rich R. Villanueva <AldwinRich90@gmail.com>
2025-09-08 14:17:59 +0800

    Create a template for the skill page section

---
+++ src/components/skill.vue
@@ -0,0 +1,51 @@
+<template>
+    <section>
+        <div>
+
+        </div>
+        <div>
+            <a href=""></a>
+        </div>
+    </section>
+    <section>
+        <div>
+            <div id="frontend" class="contents"></div>
+            <div id="frontend" class="contents"></div>
+            <div id="frontend" class="contents"></div>
+            <div id="frontend" class="contents"></div>
+        </div> 
+        <div>
+            <div id="backend" class="contents"></div>
+            <div id="backend" class="contents"></div>
+            <div id="backend" class="contents"></div>
+            <div id="backend" class="contents"></div>
+            <div id="backend" class="contents"></div>
+            <div id="backend" class="contents"></div>
+            <div id="backend" class="contents"></div>
+            
+        </div>
+    </section>
+    <footer>
+        <div> </div>
+        <div> </div>
+        <div> </div>
+        <div> </div>
+    </footer> 
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+    
+export default defineComponent({
+    setup () {
+        
+
+        return {}
+    }
+})
+</script>
+
+<style scoped>
+
+</style>

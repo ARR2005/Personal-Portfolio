3f9667ca379d0c806d2135c8621968b95625a763
Aldwin Rich R. Villanueva <AldwinRich90@gmail.com>
2025-09-10 13:57:25 +0800

    Added skills template and contents

--- src/components/skill.vue
+++ src/components/skill.vue
@@ -36,7 +36,37 @@
 <script lang="ts">
 import { defineComponent } from 'vue'
 
-    
+    const frontend = {
+        Framework:{
+            Vite:{
+                vue: "Vue",
+                react: "React"
+            }
+        },
+        Style:{
+            scss:"scss",
+            css:"css",
+            tailwind:"Tailwind"
+        }
+    }
+
+    const backend = {
+        Language : {
+            Static: "Javascript",
+            Static2:"Typescript"
+        },
+        
+        Database : {
+            Local : "Mysql",
+            Router: "Express",
+            Router1: "Laravel"
+        },
+
+        dist: {
+            node:"Node"
+        }
+    }
+
 export default defineComponent({
     setup () {
         

857f5cff6985460b7cf4a6c52eac8e36dfbe801a
Aldwin Rich R. Villanueva <AldwinRich90@gmail.com>
2025-09-10 14:23:18 +0800

    added new button variant

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -4,18 +4,18 @@
 
 <template> 
 <header class="fixed top-0 left-0 right-0 z-5 bg-transparent backdrop-blur-md border-b-1 border-grey-200 flex flex-row">
-    <div class="h-15 min-w-full flex flex-row items-baseline-last justify-around w-80 ">
-    <div class="font-extrabold w-30 size-10 text-center   ">
-            <a href="#">
+    <div class="h-15 min-w-full flex flex-row items-baseline-last justify-around w-80  ">
+        <div class="font-extrabold w-30 size-10 text-center mb-[-30px]">
+            <a href="#" class="">
                 Portfolio
             </a>
         </div>
         <div>
             <nav class="hidden md:flex space-x-2">
-                <Button variant="ghost" size="lg" id="button1"> {{ nav.Home }}</Button>
-                <Button variant="ghost" size="lg" id="button2"> {{ nav.Skill }}</Button>
-                <Button variant="ghost" size="lg" id="button3"> {{ nav.Case }}</Button>
-                <Button variant="ghost" size="lg" id="button4"> {{ nav.Contact }}</Button>
+                <Button variant="ghost" size="nav"  id="button1" class="border-"> {{ nav.Home }}</Button>
+                <Button variant="ghost" size="nav"  id="button2" class="border-"> {{ nav.Skill }}</Button>
+                <Button variant="ghost" size="nav"  id="button3" class="border-"> {{ nav.Case }}</Button>
+                <Button variant="ghost" size="nav"  id="button4" class="border-"> {{ nav.Contact }}</Button>
             </nav>
         </div>
     </div>
